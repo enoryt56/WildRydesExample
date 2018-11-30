9463eef8f9fd67c8ab6c11739ad0732d00a823d3
Tyrone Rebolledo <t.rebolledo@hotmail.com>
2018-11-30 15:42:45 +1000

    WildRydes completed

---
+++ website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_ybbFinII0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2kd98p3ppnqb47dnc45q9ftebv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://cbvxqc1d1g.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
